181d7c7db3c2b8e17a2a5a979af12249e304a9f2
hamidreza <hrhhemati@gmail.com>
2021-01-31 21:28:22 +0330

    numericSudokuMaker method added to create a only number sudoku out of input sudoku table

--- sudoko.py
+++ sudoko.py
@@ -1,3 +1,4 @@
+import re
 
 
 def extractInputFile(fileName):
@@ -6,20 +7,45 @@ def extractInputFile(fileName):
         numbers = [int(s) for s in line.split() if s.isdigit()]
         tableSize = numbers[1]
         colors = f.readline().split()
-        sudukuTable = [[0 for i in range(tableSize)] for j in range(tableSize)]
+        sudokuTable = [[0 for i in range(tableSize)] for j in range(tableSize)]
         for i in range(tableSize):
-            sudukuTable[i] = f.readline().split()
+            sudokuTable[i] = f.readline().split()
 
-    return numbers, colors, sudukuTable
+    return numbers, colors, sudokuTable
+
+def numericSudokuMaker(sudokuTable, size):
+    numericSudoku = [[0 for i in range(size)] for j in range(size)]
+    rowCounter=0
+    columnCounter=0
+    for row in sudokuTable:
+        for column in row:
+            match = re.findall("\d", column)
+            if match:
+                num = 0
+                for k in range(len(match)):
+                    num = num*10 + int(match[k])
+                numericSudoku[rowCounter][columnCounter] = num
+            else:
+                numericSudoku[rowCounter][columnCounter] = 0
+            columnCounter+=1
+        columnCounter=0
+        rowCounter+=1
+
+    return numericSudoku
+
 
 
 def main():
-
-    numbers, colors, sudukoTable = extractInputFile("2DArray.txt")
+    numbers, colors, sudokuTable = extractInputFile("2DArray.txt")
     clolorSize, TableSize = numbers
     print(clolorSize, TableSize)
     print(colors)
-    for row in sudukoTable:
+    for row in sudokuTable:
+        for column in row:
+            print(column, end=" ")
+        print()
+    numericSudoku = numericSudokuMaker(sudokuTable, numbers[1])
+    for row in numericSudoku:
         for column in row:
             print(column, end=" ")
         print()
