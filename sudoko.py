9fe4aaa49a8bd25feab2d06c7abe110cbc9f8d3a
hamidreza <hrhhemati@gmail.com>
2021-02-01 21:45:59 +0330

    Node class created, NodeMaker function developed to make a list of node objects from numeric version of sudoku table with proper domain and position

--- sudoko.py
+++ sudoko.py
@@ -1,6 +1,23 @@
 import re
 
 
+class Node:
+    def __init__(self, position, numericDomain):
+        self.position = position
+        self.numericDomain = numericDomain
+        if len(numericDomain) == 1:
+            self.hasValue = True
+        else:
+            self.hasValue = False
+
+
+    def func(self):
+        print("infrmations: ")
+        print("X Y ", self.position)
+        print("value: ", self.numericDomain)
+        print("hase value: ", self.hasValue)
+
+
 def extractInputFile(fileName):
     with open(fileName) as f:
         line = f.readline()
@@ -13,10 +30,11 @@ def extractInputFile(fileName):
 
     return numbers, colors, sudokuTable
 
+
 def numericSudokuMaker(sudokuTable, size):
     numericSudoku = [[0 for i in range(size)] for j in range(size)]
-    rowCounter=0
-    columnCounter=0
+    rowCounter = 0
+    columnCounter = 0
     for row in sudokuTable:
         for column in row:
             match = re.findall("\d", column)
@@ -26,19 +44,37 @@ def numericSudokuMaker(sudokuTable, size):
                     num = num*10 + int(match[k])
                 numericSudoku[rowCounter][columnCounter] = num
             else:
-                numericSudoku[rowCounter][columnCounter] = 0
-            columnCounter+=1
-        columnCounter=0
-        rowCounter+=1
+                numericSudoku[rowCounter][columnCounter] = "-"
+            columnCounter += 1
+        columnCounter = 0
+        rowCounter += 1
 
     return numericSudoku
 
 
+def NodeMaker(numericSudoku):
+    nodes = []
+    print(nodes)
+    i = j = 0
+    for row in numericSudoku:
+        for column in row:
+            if column == '-':
+                domain = [i+1 for i in range(len(numericSudoku))]
+            else:
+                domain = [column]
+            nodes.append(Node((i, j), domain))
+            print("i j: ", i, " ", j)
+            print("domain: ", domain)
+            print("has value: ", nodes[i*len(numericSudoku) + j].hasValue)
+            j += 1
+        j = 0
+        i += 1
+
 
 def main():
     numbers, colors, sudokuTable = extractInputFile("2DArray.txt")
-    clolorSize, TableSize = numbers
-    print(clolorSize, TableSize)
+    colorSize, TableSize = numbers
+    print(colorSize, TableSize)
     print(colors)
     for row in sudokuTable:
         for column in row:
@@ -49,5 +85,6 @@ def main():
         for column in row:
             print(column, end=" ")
         print()
+    NodeMaker(numericSudoku)
 
 main()
